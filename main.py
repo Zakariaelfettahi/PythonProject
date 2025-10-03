88e66184055bcc09b922e9f474951cbc5e93f3a6
Zakaria El fettahi <zakariaelfettahi@gmail.com>
2025-10-03 00:52:37 -0400

    Added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#this is what you get, A calculator that add two integers
+
+def add(a, b):
+    return a+b
+
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+result = add(num1, num2)
+print("The result is: ", result)
+
+#wow, such a useful project
