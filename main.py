0b9913f5e8da980feb8e92f5fa876f2df6055ab3
Zakaria El fettahi <zakariaelfettahi@gmail.com>
2025-10-03 01:30:05 -0400

    modified main.py

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-#this is what you get, A calculator that add two integers
+#A calculator with all four basic operations
 
-def add(a, b):
+def add(a: int, b: int ) -> int: #type hinting
     return a+b
 
-num1 = int(input("Number 1: "))
-num2 = int(input("Number 2: "))
-result = add(num1, num2)
-print("The result is: ", result)
+def subtract(a: int, b: int) -> int:
+    return a-b
+
+def multiply(a: int,b: int) -> int:
+    return a*b
+
+def divide(a: int, b: int) -> float:
+    return a/b
+
+print("Welcome to the most useful calculator in the world!\nWe accept integers only.\n\nFor each operation you have to enter the name of the operation in English.\n")
+operation = input("Enter operation (addition, subtraction, multiplication, division): ")
+
+if (operation!= "addition" and operation!= "subtraction" and operation!= "multiplication" and operation!= "division"): #comparative if statement
+    print("Invalid operation, Abort mission.")
+    exit() #halt execution
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+match operation: #match case syntax
+    case "addition":
+        print("The result is : ", add(num1, num2))
+    case "subtraction":
+        print("The result is: ", subtract(num1,num2))
+    case "multiplication":
+        print("The result is: ", multiply(num1,num2))
+    case "division":
+        print("The result is: ", divide(num1,num2))
+
 
 #wow, such a useful project
